cb51188adc131d81e40e179d81d7c4aeaffb1914
Wighawag <wighawag@gmail.com>
2021-03-21 15:31:03 +0000

    staging deployment

--- web/src/components/navigation/NavBar.svelte
+++ web/src/components/navigation/NavBar.svelte
@@ -5,7 +5,7 @@
   const base: string = window.basepath || '/';
 </script>
 
-<div class="absolute w-full h-1 -m-1 z-50" style={`background: url(${base}images/multicolor_line_x8.png);`}></div>
+<div class="absolute w-full h-1 -m-1 z-40" style={`background: url(${base}images/multicolor_line_x8.png);`}></div>
 <ul class="flex m-1">
   {#each links as link}
     <NavLink name={typeof link === 'string' ? link : link.name}>
@@ -13,4 +13,4 @@
     </NavLink>
   {/each}
 </ul>
-<div class="absolute w-full h-1 -m-1 z-50" style={`background: url(${base}images/multicolor_line_x8.png);`}></div>
+<div class="absolute w-full h-1 -m-1 z-40" style={`background: url(${base}images/multicolor_line_x8.png);`}></div>
