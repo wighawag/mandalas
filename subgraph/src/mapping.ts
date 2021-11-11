c39ba4dfb5b144e3c831134c64caa11b8b403abe
wighawag <wighawag@gmail.com>
2021-11-11 11:08:05 +0000

    add collected / paid

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -17,6 +17,8 @@ function handleOwnerViaId(id: string): Owner {
   entity = new Owner(id);
   entity.numMandalas = ZERO;
   entity.numMandalasMinted = ZERO;
+  entity.numCollected = ZERO;
+  entity.numSpent = ZERO;
   entity.save();
   return entity as Owner;
 }
@@ -29,6 +31,8 @@ function handleAll(): All {
     all.numMandalasMinted = ZERO;
     all.numMinters = ZERO;
     all.numOwners = ZERO;
+    all.numCollected = ZERO;
+    all.numSpent = ZERO;
   }
   return all as All;
 }
@@ -36,8 +40,11 @@ function handleAll(): All {
 function handleMandala(id : BigInt, minter: Address): Mandala {
   let mandalaId = id.toString();
   let entity = Mandala.load(mandalaId)
+
   if (!entity) {
     entity = new Mandala(mandalaId);
+    entity.numCollected = ZERO;
+    entity.numSpent = ZERO;
     entity.minter = minter.toHexString();
   }
   return entity as Mandala;
@@ -50,14 +57,18 @@ export function handleMinted(event: Minted): void {
   all.numMandalasMinted = all.numMandalasMinted.plus(ONE);
 
   let mandala = handleMandala(event.params.id, event.transaction.from);
+  mandala.numSpent = mandala.numSpent.plus(event.params.pricePaid);
   mandala.save();
 
   let owner = handleOwnerViaId(mandala.minter)
   if (owner.numMandalasMinted.equals(ZERO)) {
     all.numMinters = all.numMinters.plus(ONE);
   }
+  owner.numSpent = owner.numSpent.plus(event.params.pricePaid);
   owner.numMandalasMinted = owner.numMandalasMinted.plus(ONE);
   owner.save();
+
+  all.numSpent = all.numSpent.plus(event.params.pricePaid);
 
   all.save();
 }
@@ -67,8 +78,17 @@ export function handleBurned(event: Burned): void {
   let all = handleAll();
   all.numMandalas = all.numMandalas.minus(ONE);
   let mandala = Mandala.load(event.params.id.toString())
+
+  let owner = handleOwnerViaId(mandala.owner)
+  owner.numCollected = owner.numCollected.plus(event.params.priceReceived);
+  owner.save();
+
   mandala.owner = null;
+  mandala.numCollected = mandala.numCollected.plus(event.params.priceReceived);
   mandala.save();
+
+
+  all.numCollected = all.numCollected.plus(event.params.priceReceived);
 
   all.save();
 }
@@ -91,7 +111,7 @@ export function handleTransfer(event: Transfer): void {
     owner.save();
 
   } else if (event.params.to == ZERO_ADDRESS) {
-    mandala.owner = null;
+    // mandala.owner = null; // keep so it is handled in handleBurned
 
     let owner = handleOwnerViaId(from);
     owner.numMandalas = owner.numMandalas.minus(ONE);
