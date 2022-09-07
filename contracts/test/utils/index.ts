e78e2ea77710ec1c1799d8927ab7539422941614
wighawag <wighawag@gmail.com>
2022-09-07 11:29:42 +0100

    fix

--- contracts/test/utils/index.ts
+++ contracts/test/utils/index.ts
@@ -1,4 +1,4 @@
-import {Contract} from 'ethers';
+import {Contract, ContractReceipt} from 'ethers';
 import {ethers} from 'hardhat';
 
 export async function setupUsers<T extends {[contractName: string]: Contract}>(
@@ -22,4 +22,8 @@ export async function setupUser<T extends {[contractName: string]: Contract}>(
     user[key] = contracts[key].connect(await ethers.getSigner(address));
   }
   return user as {address: string} & T;
+}
+
+export function waitFor(p: Promise<{wait: () => Promise<ContractReceipt>}>) {
+  return p.then((v) => v.wait());
 }
