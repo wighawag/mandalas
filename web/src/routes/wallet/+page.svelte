6455b2dc4a01bb25ae933d6ef6b31f59270b7bcb
wighawag <wighawag@gmail.com>
2026-02-18 15:57:16 +0000

    todo

--- web/src/routes/wallet/+page.svelte
+++ web/src/routes/wallet/+page.svelte
@@ -3,21 +3,24 @@
   import {generateBitmapDataURI, template19_bis} from 'mandalas-common';
   import {curve, nftsof, connection} from '$lib';
 
-  let walletAddress = $state<string | undefined>(undefined);
+  let addressFromURI = $state<string | undefined>(undefined);
 
   onMount(async () => {
     // Get wallet address from URL hash
     if (typeof window !== 'undefined' && window.location.hash) {
-      walletAddress = window.location.hash.substring(1);
+      addressFromURI = window.location.hash.substring(1);
     }
   });
 
   let currentAddress = $derived($connection.step === 'WalletConnected' ? $connection.mechanism.address : undefined);
+
+  let addressToLook = $derived(addressFromURI || currentAddress);
+
   let isWalletOwner = $derived(
-    currentAddress && walletAddress && currentAddress.toLowerCase() === walletAddress.toLowerCase(),
+    currentAddress && addressToLook && currentAddress.toLowerCase() === addressToLook.toLowerCase(),
   );
 
-  let nfts = $derived(nftsof(walletAddress));
+  let nfts = $derived(nftsof(addressToLook));
 
   let connected = $derived($connection.step === 'WalletConnected' || $connection.step === 'SignedIn');
 
@@ -52,19 +55,23 @@
         Connect
       </button>
     </div>
-  {:else if !walletAddress || walletAddress === ''}
+  {:else if !addressToLook || addressToLook === ''}
     <div class="w-full h-full mx-auto text-center flex-col text-black dark:text-white">
-      <p class="mt-4 text-xs sm:text-base font-black text-yellow-400">Please Connect to your wallet see your tokens</p>
+      <p class="mt-4 text-xs sm:text-base font-black text-yellow-400">
+        Please Connect to your wallet see your tokens !
+      </p>
       <button
         class="m-2 text-xs md:text-base font-black text-yellow-400 border border-yellow-500 p-1"
         onclick={() => connection.connect({type: 'wallet'})}
       >
-        Connect
+        Connect {addressToLook}
+        {isWalletOwner}
       </button>
     </div>
   {:else if currentAddress && !isWalletOwner}
     <div class="w-full h-full mx-auto text-center flex-col text-black dark:text-white">
       <div class="w-full h-full mx-auto flex justify-between text-black dark:text-white">
+        <!-- TODO reload or reactivate s-->
         <a
           href={`#/`.concat(currentAddress)}
           class="m-2 text-xs md:text-base font-black text-yellow-400 border border-yellow-500 p-1"
@@ -82,7 +89,7 @@
           {#if isWalletOwner}
             Here are your Mandalas. You can burn them to get 95% of the current price. Each time a mandala is burnt, the
             price decrease. Note that once burnt that same Mandala cannot be re-created.
-          {:else}Here are the Mandalas for wallet {walletAddress}.{/if}
+          {:else}Here are the Mandalas for wallet {addressToLook}.{/if}
         </p>
       </div>
     {:else if currentAddress === undefined}
@@ -97,7 +104,7 @@
           <p class="p-4">You do not have any Mandala yet.</p>
           <p>get your first one <a href="/" class="underline">here</a></p>
         {:else}
-          <p class="p-4">No Mandala for {walletAddress}</p>
+          <p class="p-4">No Mandala for {addressToLook}</p>
         {/if}
       </div>
     {/if}
